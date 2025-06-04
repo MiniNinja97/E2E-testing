1603f8e639bdbc1de04e86e557ae5fc5f5e224ee
Emma <emma.malinsdotter@outlook.com>
2025-06-04 11:14:07 +0200

    la till ett test för att kolla så boken jag gillat på katalogsidan finns i min lista med sparade favoritböcker

--- src/test/läggTill.test.js
+++ src/test/läggTill.test.js
@@ -9,14 +9,20 @@ test.describe('Kollar så man kan lägga till en ny bok med text-input', () => {
 
 test('Kollar så man kan fylla i en titel', async ({ page }) => {
 
-	await page.getByTestId('add-book').click();
 
+    //gå till sidan där man lägger till en ny bok
+	await page.getByTestId('add-book').click();
+    
+	//hittar rätt inputfält
 	await page.getByTestId('add-input-title').fill('Sagan om de två tornen');
     await page.getByTestId('add-input-author').fill('J.R.R Tolkien');
-
+     
+	 //fyller i rätt inputfält
      await expect(page.getByTestId('add-input-title')).toHaveValue('Sagan om de två tornen');
      await expect(page.getByTestId('add-input-author')).toHaveValue('J.R.R Tolkien');
+      
 
+	  //klickar på lägg till knappen 
 	  await page.getByRole('Button', {name: 'Lägg till ny bok'}).click()
 
 
---
+++ src/test/minaBöcker.test.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Kollar så min gillade bok finns i min lista', () => {
+  test.beforeEach(async ({ page }) => {
+	await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/')
+
+   
+  })
+
+test('Kollar så att boken jag gillat syns i listan', async ({ page }) => {
+
+
+	
+  await page.getByTestId('star-Hur man tappar bort sin TV-fjärr 10 gånger om dagen').click();
+
+
+	//gå till sidan där ser sina gillade böcker
+	await page.getByTestId('favorites').click();
+	
+	//Kollar så boken jag gillade förut ligger i listan
+	const favorit = page.getByTestId('fav-Hur man tappar bort sin TV-fjärr 10 gånger om dagen');
+	await expect(favorit).toBeVisible();
+	await expect(favorit).toHaveText('Hur man tappar bort sin TV-fjärr 10 gånger om dagen');
+
+
+})
+
+
+})
