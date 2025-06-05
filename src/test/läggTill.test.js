4354c96b4e75e2cf9f6e27db875b4340b0fdc609
Emma <emma.malinsdotter@outlook.com>
2025-06-05 11:11:53 +0200

    la till lite ändringar i navigeringen och lägg till bok funktionen

--- src/test/läggTill.test.js
+++ src/test/läggTill.test.js
@@ -1,30 +1,31 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Kollar så man kan lägga till en ny bok med text-input', () => {
-  test.beforeEach(async ({ page }) => {
-	await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/')
 
-   
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/')
   })
 
-test('Kollar så man kan fylla i en titel', async ({ page }) => {
-
-
-    //gå till sidan där man lägger till en ny bok
-	await page.getByTestId('add-book').click();
+  test('Kollar så man kan fylla i en titel och att boken hamnar i katalogen', async ({ page }) => {
     
-	//hittar rätt inputfält
-	await page.getByTestId('add-input-title').fill('Sagan om de två tornen');
-    await page.getByTestId('add-input-author').fill('J.R.R Tolkien');
-     
-	 //fyller i rätt inputfält
-     await expect(page.getByTestId('add-input-title')).toHaveValue('Sagan om de två tornen');
-     await expect(page.getByTestId('add-input-author')).toHaveValue('J.R.R Tolkien');
-      
+    await page.getByTestId('add-book').click()
 
-	  //klickar på lägg till knappen 
-	  await page.getByRole('Button', {name: 'Lägg till ny bok'}).click()
+    
+    await page.getByTestId('add-input-title').fill('Sagan om de två tornen')
+    await page.getByTestId('add-input-author').fill('J.R.R Tolkien')
 
+   
+    await expect(page.getByTestId('add-input-title')).toHaveValue('Sagan om de två tornen')
+    await expect(page.getByTestId('add-input-author')).toHaveValue('J.R.R Tolkien')
 
-})
+   
+    await page.getByRole('button', { name: 'Lägg till ny bok' }).click()
+
+    
+    await page.getByRole('button', { name: 'Katalog' }).click()
+
+    
+    await expect(page.getByText('Sagan om de två tornen')).toBeVisible()
+    await expect(page.getByText('J.R.R Tolkien')).toBeVisible()
+  })
 })
--- src/test/navigering.test.js
+++ src/test/navigering.test.js
@@ -1,35 +1,39 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('Navigeringen', () => {
-  
-  // Körs innan varje test
+
   test.beforeEach(async ({ page }) => {
     await page.goto('https://tap-ht24-testverktyg.github.io/exam-template/')
   })
 
-  
   test('Kollar att titeln "Läslistan" visas', async ({ page }) => {
     await expect(page).toHaveTitle('Läslistan')
   })
 
-  
-  test('Kollar att förstasidan visar knapparna "Katalog", "Lägg till bok" och "Mina Böcker"', async ({ page }) => {
+  test('Kollar att knapparna "Katalog", "Lägg till bok" och "Mina böcker" finns', async ({ page }) => {
     await expect(page.getByRole('button', { name: 'Katalog' })).toBeVisible()
     await expect(page.getByRole('button', { name: 'Lägg till bok' })).toBeVisible()
-    await expect(page.getByRole('button', { name: 'Mina Böcker' })).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Mina böcker' })).toBeVisible()
   })
 
-  
-  test('Kollar att man kan klicka på "Lägg till bok"', async ({ page }) => {
+  test('Klickar på "Lägg till bok" och kollar att formuläret visas', async ({ page }) => {
     await page.getByRole('button', { name: 'Lägg till bok' }).click()
-    
-   
-    
-    
+
+    await expect(page.getByTestId('add-input-title')).toBeVisible()
   })
 
-  test('Kollar att man kan klicka på "Mina böcker" ', async({page}) => {
-    await page.getByRole('button', {name: 'Mina böcker'}).click()
-    
+  test('Klickar på "Mina böcker" och kollar att text om favoriter visas', async ({ page }) => {
+    await page.getByRole('button', { name: 'Mina böcker' }).click()
+
+    await expect(page.getByText('När du valt, kommer dina favoritböcker att visas här')).toBeVisible()
+  })
+
+  test('Klickar på "Katalog" och kollar att boklistan visas', async ({ page }) => {
+	//måste byta vy först för att "Katalog-knappen" ska gå att klicka på
+    await page.getByRole('button', { name: 'Lägg till bok' }).click()
+
+    await page.getByRole('button', { name: 'Katalog' }).click()
+    await expect(page.getByText('Hur man tappar bort sin TV-fjärr 10 gånger om dagen')).toBeVisible()
   })
 })
+
